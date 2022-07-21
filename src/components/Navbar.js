448c66c00a58baa8865399613134653ecaaccd14
rraj-03 <ritikraj1369@gmail.com>
2022-07-21 06:01:07 +0530

    final 21-07-22

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
       <Link to={"/"} className="mr-5 hover:text-gray-900">Dashboard</Link>
       
     </nav>
-    <Link to={"signup"} className="inline-flex items-center bg-indigo-500 border-0 py-1 px-3 focus:outline-none text-white hover:bg-indigo-600 rounded text-base mt-4 md:mt-0 mr-5">Sign Up
+    <div><Link to={"signup"} className="inline-flex items-center bg-indigo-500 border-0 py-1 px-3 focus:outline-none text-white hover:bg-indigo-600 rounded text-base mt-4 md:mt-0 mr-5">Sign Up
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
@@ -24,7 +24,7 @@ const Navbar = () => {
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
-    </Link>
+    </Link></div>
   </div>
 </header>
     </>
